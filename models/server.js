557db4a1ce6e494f3eeb5e42b2e93603b9463eb1
Gabplus2802 <marlo.mejia@unmsm.edu.pe>
2025-02-13 14:55:32 -0500

    Fin Seccion 9

--- models/server.js
+++ models/server.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const { dbConnection } = require('../database/config');
 
 class Server {
 
     constructor() {
         this.app = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 3000;
         this.usuariosPath = '/api/usuarios';
+        //llamada a conectarDB
+        this.conectarDB();
         //Middelware
         this.middlewares();
         //Rutas de la aplicacion
         this.routes();
+
+    }
+
+    async conectarDB() {
+        await dbConnection();
     }
 
     routes() {
